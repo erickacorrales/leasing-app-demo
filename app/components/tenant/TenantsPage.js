4810fd50f083a177d846c7c34ab9ad3296a0b5c1
Ericka Julissa Corrales Anticona <erickacorrales@gmail.com>
2017-05-10 10:02:44 +0100

    - Resolve this warning: Accessing PropTypes via the main React package is deprecated. Use the prop-types package from npm instead

--- app/components/tenant/TenantsPage.js
+++ app/components/tenant/TenantsPage.js
@@ -2,7 +2,8 @@
  * Created by erickacorrales on 2/5/17.
  */
 
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import * as tenantActions from '../../actions/tenantActions';
 
